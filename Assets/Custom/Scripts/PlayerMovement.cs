4098491ef4a1ba67d4cc0c2806a64300a994c911
er-trade <tradewise.application@gmail.com>
2026-09-07 11:19:52 +0200

    feat: making the basic player movement controller + jumping

--- Assets/Custom/Scripts/PlayerMovement.cs
+++ Assets/Custom/Scripts/PlayerMovement.cs
@@ -4,48 +4,61 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-  public CharacterController controller;
+    public CharacterController controller;
+ 
+    public float speed = 12f, jumpHeight = 3f;
+    public float gravity = -9.81f * 2.5f;
+ 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
-  public float speed = 12f;
-  public float gravity = -9.81f * 2;
-  public float jumpHeight = 3f;
+    float horizontalInput, verticalInput;
+ 
+    Vector3 velocity;
+ 
+    bool isGrounded;
 
-  public Transform groundCheck;
-  public float groundDistance = 0.4f;
-  public LayerMask groundMask;
-
-  Vector3 velocity;
-
-  bool isGrounded;
+    void Start()
+    {
+        groundMask = LayerMask.GetMask("Ground");
+    }
  
     // Update is called once per frame
-  void Update()
-  {
-    //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
-    isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-    if (isGrounded && velocity.y < 0)
+    void Update()
     {
-        velocity.y = -2f;
+        MyInput();
     }
 
-    float x = Input.GetAxis("Horizontal");
-    float z = Input.GetAxis("Vertical");
-
-    //right is the red Axis, foward is the blue axis
-    Vector3 move = transform.right * x + transform.forward * z;
-
-    controller.Move(move * speed * Time.deltaTime);
-
-    //check if the player is on the ground so he can jump
-    if (Input.GetButtonDown("Jump") && isGrounded)
+    void FixedUpdate()
     {
-        //the equation for jumping
-        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        MovePlayer();
     }
 
-    velocity.y += gravity * Time.deltaTime;
+    private void MovePlayer()
+    {
+        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+ 
+        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+ 
+        //right is the red Axis, foward is the blue axis
+        Vector3 move = transform.right * horizontalInput + transform.forward * verticalInput;
+ 
+        controller.Move(move * speed * Time.deltaTime);
+ 
+        //check if the player is on the ground so they can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity); //the equation for jumping
+ 
+        velocity.y += gravity * Time.deltaTime;
+ 
+        controller.Move(velocity * Time.deltaTime);
+    }
 
-    controller.Move(velocity * Time.deltaTime);
-  }
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
 }
